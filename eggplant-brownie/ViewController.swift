dbdb9d9fbddd4e4836a689ccb64e899492c73913
Paulo Amorim <paulorec@gmail.com>
2016-12-03 14:21:39 -0300

    :sparkles: eggplant-brownie

---
+++ eggplant-brownie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by Paulo Amorim on 03/12/16.
+//  Copyright © 2016 Paulo Amorim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
